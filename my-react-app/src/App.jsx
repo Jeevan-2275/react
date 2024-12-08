2e7d38d5ed5d3d7eeb827e688f83e98052dc66d1
Jeevan kadam$ <jeevankadam2275@gmail.com>
2024-12-08 07:55:14 +0530

    complete

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,6 +13,25 @@ import jeevan11 from './assets/img10.png';
 import jeevan12 from './assets/img11.png';
 import jeevan13 from './assets/img12.png';
 import jeevan14 from './assets/img13.png';
+import jeevan15 from './assets/img16.png';
+import jeevan16 from './assets/img17.png';
+import jeevan17 from './assets/img19.png';
+import jeevan18 from './assets/img21.png';
+import jeevan19 from './assets/img22.png';
+import jeevan20 from './assets/img23.png';
+import jeevan21 from './assets/img24.png';
+import jeevan22 from './assets/img25.png';
+import jeevan23 from './assets/img26.png';
+import jeevan24 from './assets/img27.png';
+import jeevan25 from './assets/img28.png';
+import jeevan26 from './assets/img29.png';
+
+
+
+
+
+
+
 
 
 
@@ -26,12 +45,13 @@ function App() {
                 <div className="h2">
                     <p>LOTTERY</p><p >INSTANT</p><p >CASINO</p><p >SPORTS</p><p >WINNERS</p><p >LEARN</p>
                   
-                </div><hr />
-
+                </div>
+<div className="Nav">
                 <div className="blinking-image">
                     <img src={jeevan1} alt="Blinking Image" />
                 </div>
                 <div className="blinking-text"> $80 MILLION JACKPOT </div>
+                </div>
                 <div className="color-box">LOGIN</div>
                 <div className="pop-up-box">SIGN UP</div>
                 <div><p className="en">EN</p><p className="fr">FR</p></div>
@@ -69,6 +89,7 @@ function App() {
                     <div className="pop-up-box3">CLAIM OFFER</div>
                 </div>
             </div>
+
 
             <div className="box-container4">
                 <div className="box4">
@@ -117,17 +138,181 @@ function App() {
                         <img src={jeevan13} alt="Image" className="image" />
                         <div className="popup-box box1">BUY TICKET</div>
                         <div className="popup-box box2">LEARN MORE</div>
-                        <p class="a29"> $690,000 est.</p>
+                        <p class="a29"> $690,000.Est  </p>
                     </div>
                     <div className="image-container">
                         <img src={jeevan14} alt="Image" className="image" />
                         <div className="popup-box box1">BUY TICKET</div>
                         <div className="popup-box box2">LEARN MORE</div>
-                        <p class="a30"> $690,000 est.</p>
+                        <p class="a30"> $200,000 est.</p>
                     </div>
                 </div>
-                
+                <div className="body6">
+                    <div className="image-container">
+                        <img src={jeevan15} alt="Image" className="image" />
+                        <div className="popup-box box1">BUY TICKET</div>
+                        <div className="popup-box box2">LEARN MORE</div>
+                    </div>
+                    <div><p className="a31">Up to $2,500,000</p></div>
+
+                    <div className="image-container">
+                        <img src={jeevan16} alt="Image" className="image" />
+                        <div className="popup-box box1">BUY TICKET</div>
+                        <div className="popup-box box2">LEARN MORE</div>
+
+                    </div>
+                    <div><p className="a32">$99</p></div>
+
+                    <div className="image-container">
+                        <img src={jeevan17} alt="Image" className="image" />
+                        <div className="popup-box box1">BUY TICKET</div>
+                        <div className="popup-box box2">LEARN MORE</div>
+                    </div>
+                    <div><p className="a33">50% OF PRIZE POOL</p></div> 
+
+
+
+                    
+                   
+                </div>
+
+
+<div >
+    <img className="body2" src={jeevan18}alt="" />
+</div>
+
+<div className="pop-up-box11">Enter Now</div>
+
+
+
+<p className="p1">PLAY MEGAJACKPOTS GAMES NOW!</p>
+<div className="box-container12">
+                <div className="box12">
+                    <img src={jeevan19} alt="Placeholder Image" />
+                    <h1>MEGAJACKPOTS ELEPHANT KING</h1>
+                    <h3>Your favourite classic has received the MegaJackpots treatment!</h3>
+                    <div className="pop-up-box12">PLAY NOW</div>
+               <p className="p2">Must be at least 19 years of age, a resident of Ontario and in the province at the time of play. © 2022 IGT. All rights reserved.</p>
+                </div>
             </div>
+
+            <div className="box-container13">
+                <div className="box13">
+                    <img src={jeevan23} alt="Placeholder Image" />
+                    <h1>MEGAJACKPOTS WOLF RUN</h1>
+                    <h3>Run wild with the wolves!</h3>
+               <p className="p3">PLAY NOW.</p>
+                </div>
+            </div>
+            <div className="box-container13">
+                <div className="box13">
+                    <img src={jeevan24} alt="Placeholder Image" />
+                    <h1>MEGAJACKPOTS GOLDEN GODDESS</h1>
+                    <h3>Golden Goddess meets MegaJackpots. The ancient Greek online slot game.</h3>
+               <p className="pc">PLAY NOW.</p>
+                </div>
+            </div>
+            <div className="box-container13">
+                <div className="box13">
+                    <img src={jeevan22} alt="Placeholder Image" />
+                    <h1>MEGAJACKPOTS CLEOPATRA</h1>
+                    <h3>The Queen's riches are now Mega!</h3>
+               <p className="p3">PLAY NOW.</p>
+                </div>
+            </div>
+
+
+
+
+
+             <div className="box-container14">
+                <div className="box14">
+                    <img src={jeevan21} alt="Placeholder Image" />
+                    <h1>Bring Home the Win</h1>
+                    <h3>Every play with OLG is a win for all, because 100% of profits go back to local communities, like yours.</h3>
+                    <div className="pop-up-box14">HOW WE GIVE BACK !</div>
+                </div>
+            </div>
+            <div className="box-container15">
+                <div className="box15">
+                    <img src={jeevan20} alt="Placeholder Image" />
+                    <h1>GET HELPFUL TIPS ON ODDS, STRATEGIES, AND HOW TO PLAY</h1>
+                    <h3>PlaySmart is your gambling encyclopedia, with information you need to make smart choices and keep fun in the game.</h3>
+                    <div className="pop-up-box15">LEARN MORE</div>
+                </div>
+            </div>   
+
+<div><img  className="learn"  src={jeevan25} alt="" /></div>
+<div className="pop-up-box16">LEARN MORE</div>
+
+<div className="section">
+            <h1>About</h1>
+            <ul>
+                <li>Our Company</li>
+                <li>Download OLG App</li>
+                <li>Responsible Gambling</li>
+                <li>OLG Connect</li>
+                <li>Banking</li>
+                <li>Careers</li>
+                <li>Affiliates</li>
+                <li>Retailer Hub</li>
+                <li>Become a Lottery Retailer</li>
+                <li>中文(繁體)</li>
+                <li>中文(简体)</li>
+            </ul>
+        </div>
+        <div className="section2">
+            <h1>MEDIA CENTRE</h1>
+            <ul>
+            
+                <li>OLG News</li>
+                <li>Reports & Disclosures</li>
+                <li>Player Protection</li>
+                <li>Sponsorship</li>
+                <li>OLG Sponsored Events</li>
+                <li>Community Impact</li>
+            </ul>
+            
+        </div>
+
+        <div className="section3">
+            <h1>SUPPORT</h1>
+            <ul>
+                <li>FAQs</li>
+                <li>Finding Help</li>
+                <li>My PlayBreak</li>
+                <li>Site Map</li>
+                <li>Contact Us</li>
+            </ul>
+            
+        </div>
+
+        <div className="section4">  
+        <h1>Policies</h1>
+            <ul>
+                <li>Privacy and Cookie Policy</li>
+                <li>Accessibility</li>
+                <li>Security</li>
+                <li>OLG Social Media Participation Policy</li>
+            </ul>
+        
+
+        
+                   <h1>Legal</h1>
+            <ul>
+                <li>Freedom of Information Inquiries</li>
+                <li>Website User Agreement</li>
+                <li>Player Agreement - Terms and Conditions</li>
+                <li>Responsible Gambling Policy</li>
+            </ul>
+
+            </div>
+ <div > <img className="img1" src={jeevan26 } alt="" /></div>
+  <div className="body7">
+  </div>
+  <p className="bar1">© 2024 Ontario Lottery and Gaming Corporation</p>
+<p className="bar2">8:00AM</p>
+        </div>
         </div>
             
     );
